c8f2cc709ad6bd1b53ab92921fcd842f7b3ea6b2
Adeyemi Arimoro <arimoro71@gmail.com>
2023-05-30 12:46:52 -0500

    add solution

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,16 +1,47 @@
 class Game:
+    all = []
     def __init__(self, title):
         self.title = title
         self._results = []
         self._players = []
+        Game.all.append(self)
+
+    def average_score(self, player):
+        from classes.result import Result
+        sum = 0
+        score_list = [result.score for result in Result.all if (result.game == self and  result.player ==player)]
+        all_results = [result for result in Result.all if (result.game == self and  result.player ==player)]
+        for score in score_list:
+            sum += score
+        try:
+            average = sum / len(all_results)
+            return average
+        except ValueError:
+            raise ValueError ("denominator can not be zero")
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if type(value) == str and len(value) >0 and not hasattr(self, "title"):
+            self._title = value
+        else:
+            raise ValueError("Exception")
+
         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if isinstance(new_result, Result):
+            self._results.append(new_result)
+        return [result for result in Result.all if result.game ==self] 
     
     def players(self, new_player=None):
+        from classes.result import Result
         from classes.player import Player
-        pass
-    
-    def average_score(self, player):
-        pass
+        if isinstance(new_player, Player):
+             self._players.append(new_player)
+        return [result.player for result in Result.all if result.game ==self] 
+
+   
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -6,20 +6,47 @@ class Player:
         self.username = username
         self._results = []
         self._games_played = []
+        Player.all.append(self)
+
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, value):
+        if (type(value) == str and 2 <= len(value)<= 16):
+            self._username = value
+        else:
+            raise ValueError("Exception")
         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if isinstance(new_result, Result):
+            self._results.append(new_result)
+        return [result for result in Result.all if result.player ==self] 
+        
+        
+
     
     def games_played(self, new_game=None):
         from classes.game import Game
-        pass
+        from classes.result import Result
+        if isinstance(new_game, Game):
+            self._games_played.append(new_game)
+        return [result.game for result in Result.all if result.player == self] 
+
+        
     
     def played_game(self, game):
-        pass
+        from classes.result import Result
+        return len([result.game for result in Result.all if result.player == self and result.game == game]) >0
     
     def num_times_played(self, game):
-        pass
+        from classes.result import Result
+        return len([result.game for result in Result.all if result.player == self and result.game == game])
+       
+     
     
     @classmethod
     def highest_scored(cls, game):
